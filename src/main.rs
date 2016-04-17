9632d6b874599a5b0687ebefa8304fbf7e1b2848
York Xiang <bombless@126.com>
2016-04-17 09:58:18 +0000

    Token generator

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,35 @@
 #[macro_use] extern crate nickel;
+extern crate rand;
+
 
 use nickel::Nickel;
+use rand::random;
 
 fn main() {
+    use std::collections::HashMap;
+    use std::sync::{ Arc, Mutex };
+    
+
+    let tokens = Arc::new(Mutex::new(HashMap::<u64, ()>::new()));
+    let tokens_write = tokens.clone();
     let mut server = Nickel::new();
 
     server.utilize(router! {
-        get "**" => |_req, _res| {
-            "Hello world!"
+        get "/new" => |_req, _res| {
+            let token = random();
+            tokens_write.lock().unwrap().insert(token, ());
+            token.to_string()
         }
     });
 
-    server.listen("127.0.0.1:6767");
+
+
+    server.utilize(router! {
+        get "/list" => |_req, _res| {
+            tokens.lock().unwrap().keys().map(ToString::to_string).collect::<Vec<_>>().join(",")
+        }
+    });
+
+
+    server.listen("0.0.0.0:6767");
 }
